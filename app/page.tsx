f0a93ad1dffc2ac9622260bf01f64406efb3b7ab
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-18 18:31:16 -0600

    Add layout and prototype UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,333 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+import RICARDO_IMG from "@/public/images/ricardo-1.jpg";
+
+import {
+  IoEllipsisHorizontal,
+  IoSettings,
+  IoSend,
+  IoChevronBack,
+  IoMenu,
+  IoChevronUp,
+  IoAdd,
+  IoSearch,
+} from "react-icons/io5";
+
+/**
+ * Represents an individual user type
+ * name -> The name of the user
+ * profileImage -> The image of the user, StaticImageData
+ */
+type User = {
+  name: string;
+  profileImage: StaticImageData;
+};
+
+function IndividualChat({ name, profileImage }: User) {
+  return (
+    <div className="w-full h-16 flex flex-row items-center gap-4 px-4">
+      <Image
+        src={profileImage}
+        alt="User image"
+        width={44}
+        height={44}
+        className="rounded-full"
+      />
+      <p className="text-gray-950 font-medium text-lg">{name}</p>
+    </div>
+  );
+}
 
 export default function Home() {
+  const [isMoreOptionsOpened, setIsMoreOptionsOpened] = useState(false);
+  const [isMenuOptionsOpened, setIsMenuOptionsOpened] = useState(false);
+  const [isSettingsOpened, setIsSettingsOpened] = useState(false);
+  const [isAddUserOpened, setIsAddUserOpened] = useState(false);
+
+  const handleOnMoreOptionsOpenedClick = () => {
+    setIsMoreOptionsOpened(!isMoreOptionsOpened);
+  };
+
+  const handleOnMenuOptionsButtonClick = () => {
+    setIsMenuOptionsOpened(!isMenuOptionsOpened);
+  };
+
+  const closeMenuOptions = () => {
+    setIsMenuOptionsOpened(false);
+  };
+
+  const handleOnSettingsButtonClick = () => {
+    setIsSettingsOpened(!isSettingsOpened);
+  };
+
+  const handleOnSettingsClose = () => {
+    setIsSettingsOpened(false);
+  };
+
+  const handleOnAddUserButtonClick = () => {
+    setIsAddUserOpened(!isAddUserOpened);
+  };
+
+  const handleOnAddUserClose = () => {
+    setIsAddUserOpened(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="max-h-screen h-screen w-full bg-gray-100/95 xl flex flex-row">
+      {/* Settings modal */}
+      {isSettingsOpened && (
+        <section className="absolute top-0 right-0 w-full h-full flex flex-col justify-center items-center z-10 backdrop-brightness-50">
+          <div className="h-max w-56 bg-gray-200 flex flex-col items-center divide-y">
+            <button className="h-16 w-full bg-gray-300">Log out</button>
+            <button
+              className="h-16 w-full bg-gray-300"
+              onClick={handleOnSettingsClose}
+            >
+              Close Settings
+            </button>
+          </div>
+        </section>
+      )}
+
+      {/* Add user */}
+      {isAddUserOpened && (
+        <section className="absolute top-0 right-0 w-full h-full flex flex-col justify-center items-center z-10 backdrop-brightness-50">
+          <div className="h-56 w-56 bg-gray-200 flex flex-col items-center divide-y">
+            <form className="w-full h-full p-4 flex flex-col justify-between">
+              <label>
+                <p>Enter username:</p>
+                <input type="text" className="w-full py-2 px-4" />
+              </label>
+
+              <div className="flex flex-row justify-between items-center gap-4">
+                <button
+                  className="flex-1 py-3 bg-blue-500"
+                  type="button"
+                  onClick={handleOnAddUserClose}
+                >
+                  Cancel
+                </button>
+                <button className="flex-1 py-3 bg-blue-500" type="submit">
+                  Add
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
+      )}
+
+      {/* Left section, smaller devices */}
+      {isMenuOptionsOpened && (
+        <section className="absolute lg:hidden w-full h-full backdrop-brightness-50">
+          <div className="sm:w-96 w-full h-full border-r border-zinc-600 bg-gray-800 flex flex-col">
+            {/* Menu options */}
+            <div className="h-16 flex-none w-full flex items-center justify-between border-b px-4">
+              <button className="p-1 border" onClick={closeMenuOptions}>
+                <IoChevronBack size={28} />
+              </button>
+              <p className="text-xl font-medium text-gray-950">Chats</p>
+              <button
+                className="p-1 border"
+                onClick={handleOnAddUserButtonClick}
+              >
+                <IoAdd size={28} />
+              </button>
+            </div>
+
+            {/* Chat container */}
+            <div className="flex-1 max-h-max bg-gray-300 w-full overflow-auto divide-zinc-400 flex flex-col">
+              {/* Search container */}
+              <div className="h-14 w-full">
+                <form className="relative w-full h-full p-2">
+                  <input type="text" className="w-full h-full px-4" />
+                  <button className="absolute top-0 right-0 p-3" type="submit">
+                    <IoSearch size={32} />
+                  </button>
+                </form>
+              </div>
+
+              {/* Global container */}
+              <div>
+                <p>Global</p>
+                <IndividualChat
+                  name="Ricardo Milos"
+                  profileImage={RICARDO_IMG}
+                />
+              </div>
+              {/* Personal Container */}
+              <div>
+                <p>Personal</p>
+                <IndividualChat
+                  name="Ricardo Milos"
+                  profileImage={RICARDO_IMG}
+                />
+                <IndividualChat
+                  name="Ricardo Milos"
+                  profileImage={RICARDO_IMG}
+                />
+                <IndividualChat
+                  name="Ricardo Milos"
+                  profileImage={RICARDO_IMG}
+                />
+              </div>
+            </div>
+
+            {/* Left section footer */}
+            <div className="w-full flex-none h-16 flex flex-row items-center justify-between px-4">
+              <div className="flex flex-row items-center gap-4">
+                <Image
+                  src={RICARDO_IMG}
+                  alt="User image"
+                  width={44}
+                  height={44}
+                  className="rounded-full"
+                />
+                <p className="text-gray-950 font-medium text-lg">
+                  Ricardo Milos
+                </p>
+              </div>
+              <button
+                className="p-1 border"
+                onClick={handleOnSettingsButtonClick}
+              >
+                <IoSettings size={28} />
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+
+      {/* Left section, larger devices */}
+      <section className="hidden lg:block w-96 xl:w-[32rem] h-full backdrop-brightness-50">
+        <div className="w-full h-full border-r border-zinc-600 bg-gray-800 flex flex-col">
+          {/* Menu options */}
+          <div className="h-16 flex-none w-full flex items-center justify-between border-b px-4">
+            <p className="text-xl font-medium text-gray-950">Chats</p>
+            <button className="p-1 border" onClick={handleOnAddUserButtonClick}>
+              <IoAdd size={28} />
+            </button>
+          </div>
+
+          {/* Chat container */}
+          <div className="flex-1 max-h-max bg-gray-300 w-full overflow-auto divide-zinc-400 flex flex-col">
+            {/* Search container */}
+            <div className="h-14 w-full">
+              <form className="relative w-full h-full p-2">
+                <input type="text" className="w-full h-full px-4" />
+                <button className="absolute top-0 right-0 p-3" type="submit">
+                  <IoSearch size={32} />
+                </button>
+              </form>
+            </div>
+
+            {/* Global container */}
+            <div>
+              <p>Global</p>
+              <IndividualChat name="Ricardo Milos" profileImage={RICARDO_IMG} />
+            </div>
+            {/* Personal Container */}
+            <div>
+              <p>Personal</p>
+              <IndividualChat name="Ricardo Milos" profileImage={RICARDO_IMG} />
+              <IndividualChat name="Ricardo Milos" profileImage={RICARDO_IMG} />
+              <IndividualChat name="Ricardo Milos" profileImage={RICARDO_IMG} />
+            </div>
+          </div>
+
+          {/* Left section footer */}
+          <div className="w-full flex-none h-16 flex flex-row items-center justify-between px-4">
+            <div className="flex flex-row items-center gap-4">
+              <Image
+                src={RICARDO_IMG}
+                alt="User image"
+                width={44}
+                height={44}
+                className="rounded-full"
+              />
+              <p className="text-gray-950 font-medium text-lg">Ricardo Milos</p>
+            </div>
+            <button
+              className="p-1 border"
+              onClick={handleOnSettingsButtonClick}
+            >
+              <IoSettings size={28} />
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Left section, larger devices */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Main section, right side */}
+      <section className="h-full w-full flex flex-col">
+        {/* Main header */}
+        <div className="flex-none h-16 w-full bg-gray-200 flex flex-row items-center justify-between px-4">
+          {/* User info */}
+          <div className="flex flex-row items-center gap-2 justify-between">
+            {/* Menu button */}
+            <button
+              className="bg-gray-400 mr-4 p-1 lg:hidden"
+              onClick={handleOnMenuOptionsButtonClick}
+            >
+              <IoMenu size={28} />
+            </button>
+            {/* Image */}
+            <Image
+              src={RICARDO_IMG}
+              alt="User image"
+              width={44}
+              height={44}
+              className="rounded-full"
+            />
+            <p className="text-gray-950 font-medium text-lg">Ricardo Milos</p>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* More options */}
+          <button
+            className="bg-gray-400 p-1"
+            onClick={handleOnMoreOptionsOpenedClick}
+          >
+            {isMoreOptionsOpened ? (
+              <IoChevronUp size={28} />
+            ) : (
+              <IoEllipsisHorizontal size={28} />
+            )}
+          </button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {/* More Options settings */}
+          {isMoreOptionsOpened && (
+            <div className="absolute top-16 right-0 w-full sm:w-96 h-max border bg-gray-50">
+              <button className="h-12 w-full bg-gray-100">
+                <p>Delete chat</p>
+              </button>
+            </div>
+          )}
+        </div>
+        {/* Main body */}
+        <div className="flex-1 w-full overflow-auto bg-gray-300 divide-y">
+          <div className="bg-gray-500 w-full h-96" />
+          <div className="bg-gray-500 w-full h-96" />
+          <div className="bg-gray-500 w-full h-96" />
+        </div>
+        {/* Main footer */}
+        <div className="flex-none h-16 w-full px-4 py-2">
+          <form className="w-full h-full flex flex-row items-center gap-6">
+            <input
+              type="text"
+              className="w-full h-full rounded-lg px-4 shadow-lg border border-zinc-600 "
+              placeholder="Message..."
+            />
+            <button className="flex items-center px-4 bg-gray-100 h-full border border-zinc-600 rounded-md">
+              <IoSend size={20} />
+            </button>
+          </form>
+        </div>
+      </section>
     </main>
   );
 }
